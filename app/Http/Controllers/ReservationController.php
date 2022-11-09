89cd86411c6c90977e78b7c642d5dcd2173b6bc0
Fatima Marzouq <fatima@softylus.com>
2022-11-09 10:49:45 +0200

    first commit

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\reservation;
+use Illuminate\Http\Request;
+
+class ReservationController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+    public function allReservationEP(){
+
+        $reservation = reservation::paginate(15);
+        return $reservation;
+    }
+}
